7aac96c9d89f06df5232d42e094938c5eca6ea56
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-07-27 21:15:35 +0300

    Started creating about page

--- src/components/MainSection/About.js
+++ src/components/MainSection/About.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useEffect, useRef } from 'react'
+import React, { useLayoutEffect, useEffect, useRef, useState } from 'react'
 import { Chapter, TextArea, Link } from '../Helpers/DesignAssistants'
 import { width } from '../Helpers/Helpers'
 import { SSS } from './styles'
@@ -6,11 +6,13 @@ import { SSS } from './styles'
 export default () => {
   const aboutKeyWest = useRef(null)
   const aboutWebsite = useRef(null)
+  const [current, setCurrent] = useState('KeyWest')
+  const [aboutContent, setAboutContent] = useState(null)
   useLayoutEffect(() => {
-    
-  })
+    current === 'KeyWest' ? setAboutContent(<AboutKeyWest/>) : setAboutContent(<AboutWebsite/>)
+  }, [current])
+  useEffect(() => window.addEventListener('keyup', switchSection), [])
   useEffect(() => {
-    window.addEventListener('keyup', switchSection)
   })
 
   const switchSection = (event) => {
@@ -19,30 +21,48 @@ export default () => {
         aboutWebsite.current.focus()
         aboutKeyWest.current.classList.remove('activeTabLink')
         aboutWebsite.current.classList.add('activeTabLink')
+        setCurrent(aboutWebsite.current.id)
       } else {
         aboutKeyWest.current.focus()
         aboutWebsite.current.classList.remove('activeTabLink')
         aboutKeyWest.current.classList.add('activeTabLink')
+        setCurrent(aboutKeyWest.current.id)
       }
     }
   } 
+
+  const changeActiveElement = (event) => {
+    let buttons = Array.from(document.getElementsByClassName('tabLinks'))
+    buttons.forEach(button => button.classList.remove('activeTabLink'))
+    event.target.classList.add('activeTabLink')
+    setCurrent(event.target.id)
+  }
 
   return (
     <div style={SSS()}>
       <Chapter additionalStyle={{textAlign: 'left', marginLeft: '2%'}}>About</Chapter>
       <div className="tab">
-        <button className="tabLinks activeTabLink" ref={aboutKeyWest} onClick={changeActiveElement}>Key West</button>
-        <button className="tabLinks" ref={aboutWebsite} onClick={changeActiveElement}>This Website</button>
+        <button className="tabLinks activeTabLink" id="KeyWest" ref={aboutKeyWest} onClick={changeActiveElement}>Key West</button>
+        <button className="tabLinks" id="Website" ref={aboutWebsite} onClick={changeActiveElement}>This Website</button>
       </div>
       <div className="aboutContent">
-        
+        {aboutContent}
       </div>
     </div>
   )
 }
 
-const changeActiveElement = (event) => {
-  let buttons = Array.from(document.getElementsByClassName('tabLinks'))
-  buttons.forEach(button => button.classList.remove('activeTabLink'))
-  event.target.classList.add('activeTabLink')
+const AboutWebsite = () => {
+  return (
+    <>
+      <Chapter>In brief about this website</Chapter>
+    </>
+  )
+}
+
+const AboutKeyWest = () => {
+  return (
+    <>
+    </>
+  )
 }
