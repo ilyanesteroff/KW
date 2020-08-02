ee3fbd8530b037bc8c3a36d47c2c900f685c838c
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-08-02 17:57:29 +0300

    *now

--- src/components/Helpers/Modal.js
+++ src/components/Helpers/Modal.js
@@ -1,30 +1,26 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const modalRoot = document.getElementById('modal-root');
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.el = document.createElement('div');
-  }
 
-  componentDidMount() {
-    modalRoot.appendChild(this.el)
+export default props => {
+  let element = document.createElement('div')
+
+  useLayoutEffect(() => {
+    modalRoot.appendChild(element)
     document.body.style.overflowY = 'hidden'
-  }
+    
+    return _ => {
+      modalRoot.removeChild(element)
+      document.body.style.overflowY = 'auto'
+    }
+  }, [])
 
-  componentWillUnmount() {
-    modalRoot.removeChild(this.el);
-    document.body.style.overflowY = 'auto'
-  }
-
-  render() {
-    return ReactDOM.createPortal(
-      this.props.children,
-      this.el
-    );
-  }
+  return  ReactDOM.createPortal(
+    props.children,
+    element
+  );
 }
 
 export { modalRoot }
