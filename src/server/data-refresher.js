6be3994f4bed42332d8e218da09dc1a7927a93c8
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-08-22 22:31:54 +0300

    data-refresher cleanup

--- src/server/data-refresher.js
+++ src/server/data-refresher.js
@@ -1,8 +1,4 @@
-const fs = require('fs')
-const https = require('https')
 const WorkerPool = require('./worker-pool.js')
-const worker = require('worker_threads')
-const crypto = require('crypto')
 
 const updateOrLoad = data => {
   const pool = new WorkerPool(data.length, 'requester.js')
@@ -13,16 +9,5 @@ const updateOrLoad = data => {
   })
   setTimeout(() => pool.close(), 10000)
 }
-
-/*const stream = fs.createReadStream('./data/chart.json')
-const chunks = []
-stream.on('data', (chunk) => chunks.push(chunk))
-stream.on('end', () => {
-  let json = JSON.stringify(chunks.toString())
-  const writer = fs.createWriteStream('./data/chart.json')
-  writer.end(json)
-  //console.log(JSON.parse(json))
-})*/
-
 
 module.exports = updateOrLoad
