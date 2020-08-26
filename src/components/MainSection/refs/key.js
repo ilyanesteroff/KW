5205785fc836749e9b5a653c351788e3135cf0c7
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-08-26 23:03:57 +0300

    q

---
+++ src/components/MainSection/refs/key.js
@@ -0,0 +1,4 @@
+let serverKey = 'c585cb0f5956c0a0ae78e9e3fe40edc143e01d6e98c7850ec1a25779563d43fc'
+let serverUrl = 'https://cors-anywhere.herokuapp.com/https://nameless-reaches-85690.herokuapp.com'
+
+export { serverKey, serverUrl }
