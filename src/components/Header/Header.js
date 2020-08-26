794c8ac3528db4b3b20176b5b74569e92daf7a5b
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-08-27 00:17:00 +0300

    d

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ export default props => {
         <li><UpperOption sentence={"Covid-19 Updates"} icon={faVirus} link={'/covid'}/></li>
         <li><UpperOption sentence={"Breaking News"} icon={faNewspaper} link={'/news'}/></li>
         {Width() < 1000 && <li><UpperOption sentence={"Weather"} icon={faCloud} link={'/weather'}/></li>}
-        {Width() < 1000 && <li><UpperOption sentence={"Tweets"} icon={faTwitter} link={'/twitts/KeyWest'}/></li>}
+        {Width() < 1000 && <li><UpperOption sentence={"Tweets"} icon={faTwitter} link={'/tweets/KeyWest'}/></li>}
       </UpperSection>
       <LowerSection image={props.image}/>
     </div>
